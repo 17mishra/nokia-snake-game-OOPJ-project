4d0524adca80ccee40bafb894dda95bc57cfaf4a
Utkarsh1707 <utkarsh1707@gmail.com>
2021-07-24 15:27:25 +0530

    my project

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+	//MADE by-  UTKARSH MISHRA 
+	//USN - 4NI19IS110
+	// OOPJ PROJECT 
+	// ISE (B-SECTION)
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game newGame = new Game();
+
+	}
+
+}
